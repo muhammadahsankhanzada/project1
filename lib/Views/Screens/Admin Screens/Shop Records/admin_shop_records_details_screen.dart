7ef0c143c7bb51c800cc03cb8fac14f2a0e5243b
muhammadahsankhanzada <muhammadahsankhanzadarajput@gmail.com>
2024-10-07 17:59:23 +0500

    Updated

--- lib/Views/Screens/Admin Screens/Shop Records/admin_shop_records_details_screen.dart
+++ lib/Views/Screens/Admin Screens/Shop Records/admin_shop_records_details_screen.dart
@@ -13,7 +13,7 @@ class AdminShopRecordsDetailsScreen extends StatelessWidget {
       'Revenue': '50,000',
       'Type': 'General Store',
       'Timings': '8 AM to 10 PM',
-      'Phone Number': '0302-8382845',
+      'Contact': '0302-8382845',
       'Email': 'info@communitystore.com',
       'Rating': 4.5,
     };
@@ -30,84 +30,98 @@ class AdminShopRecordsDetailsScreen extends StatelessWidget {
         backgroundColor: AppColors.green,
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Column(
-            children: [
-              SizedBox(height: 20),
-              Row(
+        child: Column(
+          children: [
+            Container(
+              padding:
+                  EdgeInsets.only(top: 30, left: 15, right: 20, bottom: 30),
+              margin: EdgeInsets.only(right: 25),
+              decoration: BoxDecoration(
+                color: AppColors.white.withOpacity(.7),
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
                 children: [
-                  Icon(
-                    Icons.store,
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.store,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'General Store',
+                        style: AppTextStyles.nameHeadingTextStyle(size: 20),
+                      ),
+                    ],
                   ),
-                  SizedBox(width: 10),
-                  Text(
-                    'General Store',
-                    style: AppTextStyles.nameHeadingTextStyle(size: 20),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'A community-focused retail establishment located in the heart of the neighborhood, offering a diverse selection of groceries, household supplies, and personal care items, catering to all your daily needs.',
+                      textAlign: TextAlign.justify,
+                      style:
+                          AppTextStyles.belowMainHeadingTextStyle(fontSize: 15),
+                    ),
                   ),
                 ],
               ),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'A community-focused retail establishment located in the heart of the neighborhood, offering a diverse selection of groceries, household supplies, and personal care items, catering to all your daily needs.',
-                  style: AppTextStyles.belowMainHeadingTextStyle(fontSize: 15),
-                ),
-              ),
-              SizedBox(height: 20),
-              Divider(
-                color: AppColors.grey,
-              ),
-              SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: AppColors.white.withOpacity(.7),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        for (var key in storeDetails.keys)
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: Text(
-                              '${key}:',
-                              style:
-                                  AppTextStyles.nameHeadingTextStyle(size: 15),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(children: [
+                SizedBox(height: 20),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    // color: AppColors.white.withOpacity(.7),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          for (var key in storeDetails.keys)
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                '${key}:',
+                                style: AppTextStyles.nameHeadingTextStyle(
+                                    size: 15),
+                              ),
                             ),
-                          ),
-                      ],
-                    ),
-                    SizedBox(width: 20),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        for (var value in storeDetails.values)
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: Text(
-                              value.toString(),
-                              style: AppTextStyles.belowMainHeadingTextStyle(
-                                  fontSize: 15),
+                        ],
+                      ),
+                      SizedBox(width: 20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          for (var value in storeDetails.values)
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                value.toString(),
+                                style: AppTextStyles.belowMainHeadingTextStyle(
+                                    fontSize: 15),
+                              ),
                             ),
-                          ),
-                      ],
-                    )
-                  ],
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              Divider(
-                color: AppColors.grey,
-              ),
-              SizedBox(height: 10),
-              Text(''),
-              SizedBox(height: 30),
-            ],
-          ),
+                SizedBox(height: 20),
+                Divider(
+                  indent: 50,
+                  endIndent: 50,
+                  color: AppColors.grey,
+                ),
+                SizedBox(height: 30),
+              ]),
+            )
+          ],
         ),
       ),
     );
